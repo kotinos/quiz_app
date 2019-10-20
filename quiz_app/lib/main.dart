359322242abff0d36e25666ebd5e89fc0190abea
kotinos <aaronchanlin@gmail.com>
2019-10-20 10:22:07 -0400

    Created body and answers.

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'),);
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s is your favorite animal?'
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text('The questions!'),
+            RaisedButton(child: Text('Answer 1'), onPressed: null),
+            RaisedButton(child: Text('Answer 2'), onPressed: null),
+            RaisedButton(child: Text('Answer 3'), onPressed: null),
+          ],
+        ),
+      ),
+    );
   }
 }
