6bac8e48ba2bc83dd5933b57e20f7de379faf033
kotinos <aaronchanlin@gmail.com>
2019-10-20 10:38:51 -0400

    Added raisedButtons.

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -3,6 +3,13 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    questionIndex = questionIndex + 1;
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
     var questions = [
@@ -17,10 +24,21 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('The questions!'),
-            RaisedButton(child: Text('Answer 1'), onPressed: null),
-            RaisedButton(child: Text('Answer 2'), onPressed: null),
-            RaisedButton(child: Text('Answer 3'), onPressed: null),
+            Text(questions[questionIndex]),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: () => print('Answer 1 chosen!'),
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () {
+                print('Answer 2 chosen!');
+              },
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQuestion,
+            ),
           ],
         ),
       ),
